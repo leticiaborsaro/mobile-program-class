e6a8b46362e0be8485ea5b3e4f20043f1e687c05
Letícia Borsaro <130999161+leticiaborsaro@users.noreply.github.com>
2026-02-24 16:42:59 -0300

    new App.js aula 2

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from "react" ;
+import {View, Text, Image} from 'react-native'; 
+import {useFonts} from "@expo-google-fonts/inter/userFonts";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render () {
+    return(
+      <View>
+        <Text style={{color:'#4B0082', fontSize:25, margin: 10}}>Aplicação Inicial</Text>
+        <Text>Primeiro Projeto - Padrão</Text>
+        <Image
+        source={{uri:'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhgLDCbDVccC4UCsMqxfs8mjN2gSp0NYDYJyX_U1awTi8uJRqicl-zml0EC_15iAQlxP0RIkIr66nd5XJ7vnvX_euXrQnukpC5iYPLzqgKRRraHFgw1AYfZhqtoi01Ar9VFqDszDQGzX3oN/s1600/003+Two+Kittens+(2)-705492.jpg'}}
+        style={{width:300, height:300}}
+        />
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
